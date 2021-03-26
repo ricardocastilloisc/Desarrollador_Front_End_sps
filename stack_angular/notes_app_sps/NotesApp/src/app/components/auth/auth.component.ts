70073a838492043940bb341a6efbe295b32d8980
ricardocastillo <ricardocastilloisc@gmail.com>
2021-03-26 11:40:03 -0500

    solucionar el problema de los cors

--- stack_angular/notes_app_sps/NotesApp/src/app/components/auth/auth.component.ts
+++ stack_angular/notes_app_sps/NotesApp/src/app/components/auth/auth.component.ts
@@ -1,28 +1,61 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  styleUrls: ['./auth.component.css'],
 })
 export class AuthComponent implements OnInit {
-
   loginForm: FormGroup;
 
-  constructor() { }
+  loggedIn = false;
+  errorLogin = false;
+
+  constructor(private api: AuthService, private route: Router) {}
 
   ngOnInit(): void {
     this.loginForm = new FormGroup({
-      email: new FormControl(null, [Validators.required, Validators.email, Validators.minLength(6)]),
-      password: new FormControl(null, [Validators.required, Validators.minLength(3)])
-    })
-  }
-  
-  onSubmit() {
-    if(this.loginForm.invalid) {
-      return;
-    }
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.email,
+        Validators.minLength(6),
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+    });
   }
 
+  onSubmit() {
+    if (!this.loginForm.invalid) {
+      const user = {
+        password: this.loginForm.value.password,
+        email: this.loginForm.value.email,
+      };
+      this.api.loginUser(user);
+      this.api.isUserLoggedIn.subscribe((val) => {
+        this.loggedIn = val;
+        new Promise((resolve) => {
+          const intervalo = setInterval(() => {
+            if (this.loggedIn) {
+              resolve('ok');
+              clearInterval(intervalo);
+            }
+          }, 100);
+        })
+          .then(() => {
+            this.route.navigate(['/notes']);
+          })
+          .catch((err) => {
+            this.errorLogin = err;
+          });
+      });
+    }else{
+      return
+    }
+  }
 }
--- stack_angular/notes_app_sps_back_end/app.js
+++ stack_angular/notes_app_sps_back_end/app.js
@@ -15,6 +15,15 @@ mongoose
 
 mongoose.set('useCreateIndex', true);
 const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
 app.use(express.json())
 app.use(express.urlencoded({extended:true}));
 app.use('/servicio/api_notes_app/users', usuario);
